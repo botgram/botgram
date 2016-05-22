17c2c26e7b2c90b610575ce9c96544fa9e9165fc
Xavier Mendez <me@jmendeth.com>
2016-05-22 11:48:50 +0200

    Better state the intent of flush

--- examples/flush.js
+++ examples/flush.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Bot that just discards all queued messages and exits.
+// Bot that just discards all queued updates and exits.
 // Useful when developing, or just to test the auth token.
 // Usage: ./flush.sh <auth token>
 
@@ -7,7 +7,7 @@ var botgram = require("..");
 var bot = botgram(process.argv[2], {timeout: 1});
 
 bot.on("ready", function () {
-  console.log("Authenticated, discarding messages...");
+  console.log("Authenticated, discarding updates...");
 });
 
 bot.on("synced", function () {
