botgram/botgram

commit 8d968d20fd8d7252bac43f52a0818dd4fcd3659b

Some refactoring

lib/reply.js
@@ -129,32 +129,32 @@ ReplyQueue.prototype.html = function html(text) {
 };
 
 ReplyQueue.prototype.photo = function photo(file, caption) {
-  if (file instanceof model.File) file = file.id;
+  file = model.resolveFile(file);
   return sendGeneric.call(this, "sendPhoto", {photo: file, caption: caption});
 };
 
 ReplyQueue.prototype.audio = function audio(file, duration, performer, title, caption) {
-  if (file instanceof model.File) file = file.id;
+  file = model.resolveFile(file);
   return sendGeneric.call(this, "sendAudio", {audio: file, duration: duration, performer: performer, title: title, caption: caption});
 };
 
 ReplyQueue.prototype.document = function document(file) {
-  if (file instanceof model.File) file = file.id;
+  file = model.resolveFile(file);
   return sendGeneric.call(this, "sendDocument", {document: file});
 };
 
 ReplyQueue.prototype.sticker = function sticker(file) {
-  if (file instanceof model.File) file = file.id;
+  file = model.resolveFile(file);
   return sendGeneric.call(this, "sendSticker", {sticker: file});
 };
 
 ReplyQueue.prototype.video = function video(file, duration, caption, width, height) {
-  if (file instanceof model.File) file = file.id;
+  file = model.resolveFile(file);
   return sendGeneric.call(this, "sendVideo", {video: file, duration: duration, caption: caption, width: width, height: height });
 };
 
 ReplyQueue.prototype.voice = function voice(file, duration, caption) {
-  if (file instanceof model.File) file = file.id;
+  file = model.resolveFile(file);
   return sendGeneric.call(this, "sendVoice", {voice: file, duration: duration, caption: caption});
 };
 
@@ -195,9 +195,10 @@ ReplyQueue.prototype.action = function action(action) {
 
 // Modifiers and high-level methods
 
-ReplyQueue.prototype.reply = function reply(message) {
-  if (message instanceof model.Message) message = message.id;
-  this.parameters["reply_to_message_id"] = message;
+ReplyQueue.prototype.reply = function reply(msg) {
+  if (msg) msg = model.resolveMessage(msg);
+  else msg = undefined;
+  this.parameters["reply_to_message_id"] = msg;
   return this;
 };
 

commit 28544625d585a392372ed87bce6e30b597bde3c8

Fix regression introduced by 8d968d20fd (see #7)

lib/reply.js
@@ -4,6 +4,7 @@ var util = require("util");
 var escapeHtml = require("escape-html");
 var utils = require("./utils");
 var model = require("./model");
+var stream = require("stream");
 
 // Entry point for the reply queues facility.
 // Each reply queue is a queue of actions, where each action is
@@ -124,6 +125,11 @@ function expectTrue(result) {
 
 // Basic methods, for sending simple messages
 
+function resolveFile(file) {
+  if (utils.isBuffer(file) || utils.isReadable(file)) return file;
+  return model.resolveFile(file);
+}
+
 ReplyQueue.prototype.forward = function forward(id, chat) {
   if (id instanceof model.Message) {
     chat = id.chat;
@@ -150,32 +156,32 @@ ReplyQueue.prototype.html = function html(text) {
 };
 
 ReplyQueue.prototype.photo = function photo(file, caption) {
-  file = model.resolveFile(file);
+  file = resolveFile(file);
   return this.sendGeneric("sendPhoto", {photo: file, caption: caption});
 };
 
 ReplyQueue.prototype.audio = function audio(file, duration, performer, title, caption) {
-  file = model.resolveFile(file);
+  file = resolveFile(file);
   return this.sendGeneric("sendAudio", {audio: file, duration: duration, performer: performer, title: title, caption: caption});
 };
 
 ReplyQueue.prototype.document = function document(file) {
-  file = model.resolveFile(file);
+  file = resolveFile(file);
   return this.sendGeneric("sendDocument", {document: file});
 };
 
 ReplyQueue.prototype.sticker = function sticker(file) {
-  file = model.resolveFile(file);
+  file = resolveFile(file);
   return this.sendGeneric("sendSticker", {sticker: file});
 };
 
 ReplyQueue.prototype.video = function video(file, duration, width, height, caption) {
-  file = model.resolveFile(file);
+  file = resolveFile(file);
   return this.sendGeneric("sendVideo", {video: file, duration: duration, caption: caption, width: width, height: height });
 };
 
 ReplyQueue.prototype.voice = function voice(file, duration, caption) {
-  file = model.resolveFile(file);
+  file = resolveFile(file);
   return this.sendGeneric("sendVoice", {voice: file, duration: duration, caption: caption});
 };
 