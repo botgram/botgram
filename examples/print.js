a793d84bc72280ed80c4080c6c6869c2caa893bf
Xavier Mendez <me@jmendeth.com>
2017-04-09 15:00:34 +0200

    Improve print example to show edits too

--- examples/print.js
+++ examples/print.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Bot that prints all received messages to the console.
+// Bot that prints all received messages and edits to the console.
 // Useful when developing, or just to test the auth token.
 // Usage: ./print.sh <auth token>
 
@@ -16,11 +16,15 @@ bot.synced(function () {
   console.log("Waiting for messages...");
 });
 
-bot.all(function (msg, reply, next) {
-  var type = msg.type ? capitalize(msg.type) : "Unknown message";
+function handler(msg, reply, next) {
+  var type = msg.type ? msg.type : "unknown message";
+  type = msg.edited ? ("Edited " + type) : capitalize(type);
   console.log("\n%s at %s %s (%s):", type, msg.chat.type, msg.chat.id, msg.chat.name);
   console.log(util.inspect(msg, {colors: true, depth: null}));
-});
+}
+
+bot.all(handler);
+bot.edited.all(handler);
 
 function capitalize(s) {
   return s[0].toUpperCase() + s.slice(1);
